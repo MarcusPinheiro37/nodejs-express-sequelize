8b0030c12b002f4c7d0b7ec360e9296fa7988ac7
Marcola <marcusmagno34@gmail.com>
2024-04-08 16:17:41 -0300

    adicionada cor amarela ao log do server rodando

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./src/app.js');
-
 const PORT = 3000;
-
+const textoAmarelo = '\x1b[33m';
 app.listen(PORT, () => {
-    console.log('servidor escutando!');
+    console.log(`${textoAmarelo}servidor escutando!`);
 });
